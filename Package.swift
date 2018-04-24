85ffac55dd90f0ef614e3fccd7f4c16f21d8e0c3
Hulk <>
2018-04-24 15:17:51 +0800

    try

--- Package.swift
+++ Package.swift
@@ -2,24 +2,29 @@
 
 import PackageDescription
 
-//let package = Package(
-//    name: "hello",
-//    products: [
-//        .library(name: "App", targets: ["App"]),
-//        .executable(name: "Run", targets: ["Run"])
-//    ],
-//    dependencies: [
-//        .Package(url: "https://github.com/vapor/vapor.git",  majorVersion: 2, minor: 1),
-//        .Package(url: "https://github.com/vapor/fluent-provider.git",  majorVersion: 1, minor: 2),
-//    ],
-//    targets: [
-//        .target(
-//            name: "App",
-//            dependencies: ["Vapor", "FluentProvider"],
-//            exclude: ["Config", "Public", "Resources"]
-//        ),
-//        .target(name: "Run", dependencies: ["App"]),
-//        .testTarget(name: "AppTests", dependencies: ["App", "Testing"])
-//    ]
-//)
+let package = Package(
+    name: "test",
+    products: [
+        .library(name: "App", targets: ["App"]),
+        .executable(name: "Run", targets: ["Run"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.3.0")),
+        .package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.3.0")),
+        ],
+    targets: [
+        .target(
+            name: "App",
+            dependencies: ["Vapor", "FluentProvider"],
+            exclude: [
+                "Config",
+                "Public",
+                "Resources",
+                ]
+        ),
+        .target(name: "Run", dependencies: ["App"]),
+        .testTarget(name: "AppTests", dependencies: ["App", "Testing"])
+    ]
+)
+
 
