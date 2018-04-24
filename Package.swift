89f4e2a31d904bfd7abb39c5613ee53ddd2076f5
Hulk <>
2018-04-24 15:47:34 +0800

    tetete

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "test",
+    name: "hello",
     products: [
         .library(name: "App", targets: ["App"]),
         .executable(name: "Run", targets: ["Run"])
