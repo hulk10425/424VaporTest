312720fa7308529fc05f65be18ec3a6356123829
Hulk <>
2018-04-24 15:50:09 +0800

    sdfsdf

--- Package.swift
+++ Package.swift
@@ -9,8 +9,8 @@ let package = Package(
         .executable(name: "Run", targets: ["Run"])
     ],
     dependencies: [
-    .package(url: "https://github.com/vapor/vapor.git", version: "2.1.0"),
-        .package(url: "https://github.com/vapor/fluent-provider.git", version: "1.2.0"),
+        .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.1.0")),
+        .package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.2.0")),
         ],
     targets: [
         .target(
